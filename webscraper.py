9f2b60f615e0d0d223e4e1fd940fbd107fd3f808
StevenIGuess <cezarst42@gmail.com>
2023-03-23 16:22:24 +0000

    fixed the webscraper

--- webscraper.py
+++ webscraper.py
@@ -26,14 +26,14 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(options['url'])
 
 
-driver.find_element(by=By.XPATH, value="/html/body/div/div/div[1]/div/form/input[1]").send_keys(options['username'])
-driver.find_element(by=By.XPATH, value="/html/body/div/div/div[1]/div/form/input[2]").send_keys(options['password'])
-driver.find_element(by=By.XPATH, value="/html/body/div/div/div[1]/div/form/button").click()
+driver.find_element(by=By.NAME, value="loginname").send_keys(options['username'])
+driver.find_element(by=By.NAME, value="password").send_keys(options['password'])
+driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/div[1]/div/form/button").click()
 
 time.sleep(3)
 
 try:
-    html = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[1]/div/div/div/div[2]/div[3]/div[3]").get_attribute("innerHTML")
+    html = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/div[1]/div/div/div/div[2]/div[3]/div[3]/div").get_attribute("innerHTML")
     print(html)
 except:
     print("Could not find homework element")
@@ -45,7 +45,8 @@ print("\n\n SUCCESSFULLY RETRIEVED DATA \n\n")
 output = html_to_json.convert(html)
 
 try:
-    os.remove("./homework.json")
+    #os.remove("./homework.json")
+    pass
 except:
     pass
 
@@ -62,12 +63,10 @@ print("\n")
 print("================================")
 
 for i in output['div'][0]['div']:
-    assignment = i['div'][0]['div'][0]['b'][0]["_value"]
-    teacher = i['div'][0]['i'][0]['_value'].replace('von ', '')
-    subject = i['div'][0]['span'][0]['strong'][0]['_value']
-    date = i['div'][0]['span'][1]['_value']
-    deadline = i['div'][0]['span'][2]['_value']  
+    assignment = i['div'][0]['b'][0]["_value"]
+    teacher = i['i'][0]['_value'].replace('von ', '')
+    subject = i['span'][0]['strong'][0]['_value'] 
     print("Assignment number ", j)
-    print("{}, \n by {} from your {} class, \n from {} till {}".format(assignment, teacher, subject, date, deadline))
+    print("{}, \n by {} from your {} class".format(assignment, teacher, subject))
     print("================================")
     j += 1
