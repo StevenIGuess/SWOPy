17b1770e67c21ddbd015f4c9277394faca375e92
Stefan <cezarst42@gmail.com>
2023-03-26 21:13:33 +0200

    commit last changes

--- devbot/src/patchnotes.py
+++ devbot/src/patchnotes.py
@@ -8,4 +8,4 @@ with open("../patchnotes.txt", "r") as patchnotes:
     message = patchnotes.read()
 
 bot = telebot.TeleBot(options['telegrammapikey_dev'])
-bot.send_message(chat_id=int(options['fakechat']), text=message)
+bot.send_message(chat_id=int(options['telegramchatid']), text=message)
